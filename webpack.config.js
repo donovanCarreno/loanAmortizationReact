3be8a1ad81a82cdd7aa606abfcf61683faa2cc0d
donovanCarreno <donovan_carreno@yahoo.com>
2016-11-22 08:42:57 -0600

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src/index.jsx',
+
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  }
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'bable',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
